d6402c77fc565f1442535f98b382f9857a6fdadf
hexaredecimal <mfanakagama@gmail.com>
2024-11-23 22:03:40 +0200

    VideLanguages: Fix some missing syntaxes

--- src/com/raelity/jvi/cmd/VideLanguages.java
+++ src/com/raelity/jvi/cmd/VideLanguages.java
@@ -7,7 +7,8 @@ package com.raelity.jvi.cmd;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 
 public enum VideLanguages {
-	ACTIONSCRIPT(SyntaxConstants.SYNTAX_STYLE_ACTIONSCRIPT), ASSEMBLY(SyntaxConstants.SYNTAX_STYLE_ASSEMBLER_X86),
+	ACTIONSCRIPT(SyntaxConstants.SYNTAX_STYLE_ACTIONSCRIPT), 
+	ASSEMBLY(SyntaxConstants.SYNTAX_STYLE_ASSEMBLER_X86),
 	BBCODE(SyntaxConstants.SYNTAX_STYLE_BBCODE), C(SyntaxConstants.SYNTAX_STYLE_C),
 	CLOJURE(SyntaxConstants.SYNTAX_STYLE_CLOJURE), CPLUSPLUS(SyntaxConstants.SYNTAX_STYLE_CPLUSPLUS),
 	CSHARP(SyntaxConstants.SYNTAX_STYLE_CSHARP), CSS(SyntaxConstants.SYNTAX_STYLE_CSS),
@@ -23,7 +24,7 @@ public enum VideLanguages {
 	PYTHON(SyntaxConstants.SYNTAX_STYLE_PYTHON), RUBY(SyntaxConstants.SYNTAX_STYLE_RUBY),
 	SAS(SyntaxConstants.SYNTAX_STYLE_SAS), SCALA(SyntaxConstants.SYNTAX_STYLE_SCALA),
 	SQL(SyntaxConstants.SYNTAX_STYLE_SQL), TCL(SyntaxConstants.SYNTAX_STYLE_TCL),
-	Bash(SyntaxConstants.SYNTAX_STYLE_UNIX_SHELL), BATCH(SyntaxConstants.SYNTAX_STYLE_WINDOWS_BATCH),
+	BASH(SyntaxConstants.SYNTAX_STYLE_UNIX_SHELL), BATCH(SyntaxConstants.SYNTAX_STYLE_WINDOWS_BATCH),
 	XML(SyntaxConstants.SYNTAX_STYLE_XML), TXT(SyntaxConstants.SYNTAX_STYLE_NONE),
 	GO(SyntaxConstants.SYNTAX_STYLE_GO), RUST(SyntaxConstants.SYNTAX_STYLE_RUST), 
 	TYPESCRIPT(SyntaxConstants.SYNTAX_STYLE_TYPESCRIPT);
